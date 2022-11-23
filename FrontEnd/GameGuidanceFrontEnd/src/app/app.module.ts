36d6b255b23049be8e73db0d8774e3226ee857b7
Troy <wilson.troy.anthony@gmail.com>
2022-11-22 19:17:13 -0500

    Added most of the components

--- FrontEnd/GameGuidanceFrontEnd/src/app/app.module.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserSignupComponent } from './user-signup/user-signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { RecommendedGamesComponent } from './recommended-games/recommended-games.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    UserSignupComponent,
+    NavbarComponent,
+    AboutComponent,
+    RecommendedGamesComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ FrontEnd/GameGuidanceFrontEnd/src/app/recommended-games/recommended-games.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecommendedGamesComponent } from './recommended-games.component';
+
+describe('RecommendedGamesComponent', () => {
+  let component: RecommendedGamesComponent;
+  let fixture: ComponentFixture<RecommendedGamesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecommendedGamesComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RecommendedGamesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
