6f370a778333e23c8da89543b5c8f8c182a86ea5
Elber Funez <elberfunez@gmail.com>
2022-11-23 20:42:03 -0500

    first changes to nav added some routing as well

--- FrontEnd/GameGuidanceFrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserLoginComponent } from './components/user-login/user-login.component';
 import { UserSignupComponent } from './components/user-signup/user-signup.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AboutComponent } from './components/about/about.component';
 
 const routes: Routes = [
   {path: 'login', component: UserLoginComponent},
   {path: 'signup', component: UserSignupComponent},
+  {path: 'home', component: MainPageComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'logout', component: UserLoginComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
