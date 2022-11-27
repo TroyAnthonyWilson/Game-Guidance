8474974571c50df40dc4cc1b273de1b00e5a2e40
Troy <wilson.troy.anthony@gmail.com>
2022-11-27 14:12:51 -0500

    Added connection string

--- BackEnd/GameGuidanceAPI/GameGuidanceAPI/Program.cs
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Program.cs
@@ -1,3 +1,6 @@
+using GameGuidanceAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<GameGuidanceDBContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("sqlServerConnStr"));
+});
 
 builder.Services.AddCors(options =>
 {
