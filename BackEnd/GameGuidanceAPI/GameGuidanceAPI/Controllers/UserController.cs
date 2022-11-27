a4866400848a440ecdf00f338dc2ac49f44d390f
Troy <wilson.troy.anthony@gmail.com>
2022-11-27 16:59:13 -0500

    User controller

---
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using GameGuidanceAPI.Context;
+using GameGuidanceAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameGuidanceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly GameGuidanceDBContext _authContext;
+        public UserController(GameGuidanceDBContext gameGuidanceDBContext)
+        {
+            _authContext= gameGuidanceDBContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate ([FromBody] User userObj)
+        {
+            
+            if(userObj == null)
+                return BadRequest();
+
+            var user = await _authContext.Users.FirstOrDefaultAsync(x => x.UserName == userObj.UserName && x.Password == userObj.Password);
+
+            if(user == null)
+                return NotFound(new {Message = "User Not Found!" });
+
+            return Ok(new { Message = "Login Success!"});
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if(userObj == null )
+                return BadRequest();
+
+            if(string.IsNullOrEmpty(userObj.UserName))
+                return BadRequest();
+
+            await _authContext.Users.AddAsync(userObj);
+            await _authContext.SaveChangesAsync();
+            return Ok(new { Message = "User Registered"});
+        }
+
+    }
+}
