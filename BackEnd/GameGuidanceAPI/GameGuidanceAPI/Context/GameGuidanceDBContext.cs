761aea12b199a3fc403218bdd98dfdd046270f94
Troy <wilson.troy.anthony@gmail.com>
2022-11-27 15:36:39 -0500

    Added migration

--- BackEnd/GameGuidanceAPI/GameGuidanceAPI/Context/GameGuidanceDBContext.cs
+++ BackEnd/GameGuidanceAPI/GameGuidanceAPI/Context/GameGuidanceDBContext.cs
@@ -1,6 +1,21 @@
-﻿namespace GameGuidanceAPI.Context
+﻿using GameGuidanceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameGuidanceAPI.Context
 {
-    public class GameGuidanceDBContext
+    public class GameGuidanceDBContext : DbContext
     {
+        public GameGuidanceDBContext(DbContextOptions<GameGuidanceDBContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("users");
+        }
     }
 }
