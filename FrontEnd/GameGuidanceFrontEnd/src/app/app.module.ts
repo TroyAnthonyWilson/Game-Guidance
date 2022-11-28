3f4bae058794fbe462436f4e52723f2f9a447426
kmerck88 <Kevin.Merck88@gmail.com>
2022-11-28 16:54:35 -0500

    Test Push

--- FrontEnd/GameGuidanceFrontEnd/src/app/app.module.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
@@ -8,8 +9,8 @@ import { UserSignupComponent } from './components/user-signup/user-signup.compon
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { RecommendedGamesComponent } from './components/recommended-games/recommended-games.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -19,14 +20,20 @@ import { ReactiveFormsModule } from '@angular/forms';
     NavbarComponent,
     AboutComponent,
     RecommendedGamesComponent,
-    MainPageComponent
+
+  
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    
+  
+
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
--- FrontEnd/GameGuidanceFrontEnd/src/app/components/main-page/main-page.component.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/components/main-page/main-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
+  providers: [NgbRatingConfig]
+
+
 })
+
+
 export class MainPageComponent implements OnInit {
+ 
 
-  constructor() { }
-
+  constructor(config: NgbRatingConfig ) {
+    config.max = 5;
+    config.readonly = true;
+  }
   ngOnInit(): void {
   }
+  
 
 }
