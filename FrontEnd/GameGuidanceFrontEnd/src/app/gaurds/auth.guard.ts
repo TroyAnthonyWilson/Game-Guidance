8951db59458f8596ad75d9be901a9cbb52ec3599
Troy <wilson.troy.anthony@gmail.com>
2022-11-28 21:28:49 -0500

    Added logout

--- FrontEnd/GameGuidanceFrontEnd/src/app/gaurds/auth.guard.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/gaurds/auth.guard.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth : AuthService) { }
+  constructor(private auth : AuthService, private router: Router) { }
 
 
   canActivate(): boolean {
     if(this.auth.isLoggedIn()){
     return true;
   }else{
+    this.router.navigate(['login']);
     return false;
   }
 }
--- FrontEnd/GameGuidanceFrontEnd/src/app/services/auth.service.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class AuthService {
 
   private baseUrl:string = "https://localhost:7117/api/User/";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   signUp(userObj:any){
     return this.http.post<any>(`${this.baseUrl}register`, userObj);
@@ -28,5 +29,11 @@ export class AuthService {
 
   isLoggedIn(): boolean{
     return !!localStorage.getItem('token');
+  }
+
+  signOut(){
+    localStorage.clear();
+    localStorage.removeItem('token');
+    this.router.navigate(['login']);
   }
 }
