17d9076bfe24a6ccf4f0b02ef3e20d8b218b3c63
kmerck88 <Kevin.Merck88@gmail.com>
2022-12-01 04:50:51 -0500

    latest changes & mock favorite games

--- FrontEnd/GameGuidanceFrontEnd/src/app/app.module.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
