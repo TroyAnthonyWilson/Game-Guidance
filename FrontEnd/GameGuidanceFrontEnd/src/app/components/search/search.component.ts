412421b330cc6c633f7691026a7b7be64f6ab676
Troy <wilson.troy.anthony@gmail.com>
2022-12-07 22:35:16 -0500

    ignore list filtering search

--- FrontEnd/GameGuidanceFrontEnd/src/app/components/search/search.component.ts
+++ FrontEnd/GameGuidanceFrontEnd/src/app/components/search/search.component.ts
@@ -4,6 +4,7 @@ import { Search } from 'src/app/interfaces/gameInfo';
 import { FavoriteService } from 'src/app/services/favorite.service';
 import { SearchService } from 'src/app/services/search.service';
 import { UserFavorite } from 'src/app/interfaces/user-favorite';
+import { IgnoreService } from 'src/app/services/ignore.service';
 
 @Component({
   selector: 'app-search',
@@ -14,12 +15,15 @@ export class SearchComponent implements OnInit {
   search: string = "";
   searchResult: Search[] = [];
   favorites: UserFavorite[] = [];
+  ignores: UserFavorite[] = [];
   favoritesIds: Number[] = [];
+  ignoreIds: Number[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private favorite : FavoriteService,
     private searching : SearchService,
+    private ignore: IgnoreService,
     ) { }
 
   ngOnInit(): void {
@@ -31,7 +35,9 @@ export class SearchComponent implements OnInit {
   }
 
 loadSearchResult(){
+  console.log("loadSearchResult");
   this.getFavorites();
+  this.getIgnore();
   //console.log("favorites: " + this.favoritesIds);
 
   if(this.search == ""){
@@ -46,13 +52,41 @@ loadSearchResult(){
 }
 
  getFavorites = () : void => {
+  console.log("getFavorites");
     this.favorite.getFavorites().subscribe((data: any) => {
       this.favorites = data;
       this.getFavoritesIds();
     });
   }
 
+  getIgnore = () : void => {
+    console.log("getIgnore");   
+    this.ignore.getIgnore().subscribe((data: any) => {
+      this.ignores = data;
+      this.getIgnoreIds();
+      // this.removeAllIgnoredGamesFromSearchResult();
+    });
+  }
+
+  // removeAllIgnoredGamesFromSearchResult = () => {
+  //   console.log("removeAllIgnoredGamesFromSearchResult");
+  //   this.searchResult.forEach((game) => {
+  //     //console.log("game: " + game.id);
+  //     this.ignoreIds.forEach((id) => {
+  //       console.log("id: " + id);
+  //     });
+
+  //     if(this.ignoreIds.includes(game.id)){
+  //       const index = this.searchResult.indexOf(game);
+  //       delete this.searchResult[index];
+  //       console.log("removed: " + game.id);       
+  //     }
+  //   });
+  // }
+    
+
   getFavoritesIds = () => {
+    console.log("getFavoritesIds");    
     let ids: Number[] = [];
     this.favorites.forEach((fav) => {
       ids.push(fav.gameId);
@@ -63,21 +97,52 @@ loadSearchResult(){
     //console.log("favoritesIds: " + this.favoritesIds);   
   };
 
+  getIgnoreIds = () => {
+    console.log("getIgnoreIds");   
+    let ids: Number[] = [];
+    this.ignores.forEach((ignore) => {
+      ids.push(ignore.gameId);
+    });
+    
+    
+    this.ignoreIds = ids;
+    //console.log("ids: " + this.ignoreIds);
+  };
+
 // add to favorites
 addToFavorites(id: number){
+  console.log("favoritesIds: " + this.favoritesIds);
   console.log("Add to favorites: " + id);
   this.favorite.addfavorite(id).subscribe((data: any) => {
     //console.log(data);
     this.getFavorites();
+    this.getIgnore();
   }); 
   };
 
+  addToIgnore(id: number){
+    console.log("Add to ignore: " + id);
+    this.ignore.addIgnore(id).subscribe((data: any) => {
+      //console.log(data);
+      this.getFavorites();
+      this.getIgnore();
+    }); 
+    };
+
 // remove from favorites
 removeFromFavorites(id: number){
-  console.log("Remove from favorites: " + id);
+  console.log("Removed from favorites: " + id);
   this.favorite.removeFavorite(id).subscribe((data: any) => {
     //console.log(data);
     this.getFavorites();
   }); 
   }
+
+  removeFromIgnore(id: number){
+    console.log("Removed from ignore: " + id);
+    this.ignore.removeIgnore(id).subscribe((data: any) => {
+      //console.log(data);
+      this.getIgnore();
+    }); 
+    }
 }
---
+++ FrontEnd/GameGuidanceFrontEnd/src/app/services/ignore.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { UserFavorite } from '../interfaces/user-favorite';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IgnoreService {
+
+  private baseUrl:string = "https://localhost:7117/api/UserIgnore/";
+
+  constructor(private http: HttpClient) { }
+
+  addIgnore(Id: number){
+    return this.http.post<any>(`${this.baseUrl}addignore?gameId=${Id}`, Id);
+  } 
+
+  removeIgnore(Id: number){
+    return this.http.delete<any>(`${this.baseUrl}removeignore?gameId=${Id}`);
+  }
+
+  getIgnore= (): Observable<UserFavorite[]> => {
+    return this.http.get<any>(`${this.baseUrl}getignores`);
+  }  
+
+}
